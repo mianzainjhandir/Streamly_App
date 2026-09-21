83a0f99c40142965373f282e611a5475006d71b9
Bilal Saeed <mianzainjh@gmail.com>
2026-09-21 11:05:36 +0500

    My next step is to show splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 import 'firebase_options.dart';
 
@@ -16,7 +17,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+
       title: "Streaming App",
 
     );
