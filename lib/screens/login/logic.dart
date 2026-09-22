3da33a653f1ba1b896db8b2a03004ab3bb59a940
Bilal Saeed <mianzainjh@gmail.com>
2026-09-22 16:50:17 +0500

    I build complete logic for login screen.

---
+++ lib/screens/login/logic.dart
@@ -0,0 +1,39 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:streamly_app/screens/home.dart';
+
+class LogInController extends GetxController{
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passController = TextEditingController();
+
+  Future<bool> logIn () async{
+    String email = emailController.text.trim();
+    String password = passController.text.trim();
+
+    if(email.isEmpty || password.isEmpty){
+      Get.snackbar("Error", "All fields are required!", backgroundColor: Colors.red);
+      return false;
+    }
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+
+      Get.snackbar("Success", "Logged in Successfully!", backgroundColor: Colors.green);
+
+      // Fields Clear
+      emailController.clear();
+      passController.clear();
+
+      // Navigate to Home
+      Get.offAll(() => const HomeScreen());
+
+      return true; // ✅ Login Success
+    } catch (e) {
+      Get.snackbar("Login Failed", e.toString(), backgroundColor: Colors.red);
+      return false; // ❌ Login Failed
+    }
+  }
+}
