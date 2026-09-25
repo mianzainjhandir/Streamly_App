f7f40bf3b967e08cf44183f912f7e5ef5c78000c
Bilal Saeed <mianzainjh@gmail.com>
2026-09-25 08:56:46 +0500

    Adjusted video player height constraint to 220dp with natural AspectRatio contain fit

--- lib/screens/video_player/view.dart
+++ lib/screens/video_player/view.dart
@@ -155,24 +155,20 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
             // Video Player Container
             Container(
               width: double.infinity,
-              height: 260,
+              constraints: const BoxConstraints(
+                maxHeight: 220,
+              ),
               color: Colors.black,
               child: _isInitialized
                   ? Stack(
                       alignment: Alignment.bottomCenter,
                       children: [
-                        SizedBox.expand(
-                          child: FittedBox(
-                            fit: BoxFit.cover,
-                            child: SizedBox(
-                              width: _controller.value.size.width > 0
-                                  ? _controller.value.size.width
-                                  : 16,
-                              height: _controller.value.size.height > 0
-                                  ? _controller.value.size.height
-                                  : 9,
-                              child: VideoPlayer(_controller),
-                            ),
+                        Center(
+                          child: AspectRatio(
+                            aspectRatio: _controller.value.aspectRatio > 0
+                                ? _controller.value.aspectRatio
+                                : 16 / 9,
+                            child: VideoPlayer(_controller),
                           ),
                         ),
                         // Play/Pause Overlay & Progress Bar
